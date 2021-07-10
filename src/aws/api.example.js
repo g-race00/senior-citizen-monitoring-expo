a8167d3db5b08a8a94a3fd386f6837989aafed2a
mush00 <graceong96@outlook.com>
2021-07-10 23:18:21 +0800

    update aws api example file

--- src/aws/api.example.js
+++ src/aws/api.example.js
@@ -2,4 +2,6 @@ export const awsDbAPI = {
     userGet: 'https://.../dev/users/',
     userCreate: 'https://.../dev/users',
     userUpdate: 'https://.../dev/users/edit/',
+    alertsHist: 'https://.../dev/alerts',
+    curLoc: 'https://.../dev/current'
 };
